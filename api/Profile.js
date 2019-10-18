e7d4195d16847422e5e49c2e1803aafa7b1d54a9
Georgewisdom <coppycatter100@gmail.com>
2019-10-18 16:22:37 +0100

    check

--- api/Profile.js
+++ api/Profile.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const Profile = require("../model/Profile");
+const authenticate = require("../middleware/authenticate");
 
 // @route   GET api/account
 // @desc    Edit Profile
 // @access  Private
-router.get("/edit", (req, res) => {
-  res.status(200).json({ msg: "send" });
+router.get("/edit", async (req, res) => {
+  //   Get User
+  console.log(req.body);
 });
 
 module.exports = router;
